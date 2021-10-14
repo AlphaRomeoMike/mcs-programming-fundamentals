92241c77cbfb82e012ea18817dc85a25432d9946
Abdul Rafay Modi <subjectalpha32@gmail.com>
2021-10-14 20:46:39 +0500

    lecture three

---
+++ Programming Fundamentals Lecture 1/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Programming_Fundamentals_Lecture_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            float netSalary, grossSalary, tax, providentFund;
+            Console.WriteLine("Please enter your gross salary");
+            grossSalary = Convert.ToSingle(Console.ReadLine());
+
+            tax = grossSalary * Convert.ToSingle(0.03);
+            providentFund = grossSalary * Convert.ToSingle(0.10);
+
+            netSalary = grossSalary - tax + providentFund;
+
+            Console.WriteLine("The tax on salary " + grossSalary + " is " + tax);
+            Console.WriteLine("The provident fund on salary " + grossSalary + " is " + providentFund);
+            Console.WriteLine("The net salary is = " + netSalary);
+
+        }
+    }
+}
