f243bc6df94a2bfd888dcdff095d72d617c60e40
AlphaRomeoMike <rafaymodi18@gmail.com>
2021-11-11 02:37:24 +0500

    Added degrees to radians project

---
+++ Degree To Radians/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Degree_To_Radians
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double formula = 3.1416 / 180;
+            Console.WriteLine("Please enter your selected number");
+            double degrees = Convert.ToDouble(Console.ReadLine());
+
+            double radians = degrees * formula;
+
+            Console.WriteLine($"{degrees} degrees coverted to radians are {radians}");
+        }
+    }
+}
